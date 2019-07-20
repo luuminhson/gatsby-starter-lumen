3deff9ea3ecb822b1d98043b85dac7a6e6b55dda
Son Luu Minh <son.luu@notabasement.com>
2019-07-21 01:10:52 +0700

    Add prop onDark to component Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styles from './Button.module.scss';
 
-const PureButton = ({ link, label, className }) => (
+const PureButton = ({ link, label, className, onDark }) => (
   <div className={className} >
-    <Link className={styles['button']} to={link}>{label}</Link>
+    <Link className={[styles['button'], onDark && styles['onDark']].join(' ')} to={link}>{label}</Link>
   </div>
 );
 
