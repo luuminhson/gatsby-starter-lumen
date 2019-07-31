2cea927a9808eb4a409949b810e16c70d48f1d04
Son Luu Minh <son.luu@notabasement.com>
2019-08-01 00:22:57 +0700

    Component SectionTitle: show action button only if actionLink is available

--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -6,7 +6,7 @@ import styles from './SectionTitle.module.scss';
 const PureSectionTitle = ({ title, actionLink, actionLabel, className }) => (
     <div className={[styles['wrapper'], className].join(' ')}>
         <h2 className={styles['wrapper__title']}>{title}</h2>
-        <Button className={styles['wrapper__link']} link={actionLink} label={actionLabel} onDark />
+        { actionLink && <Button className={styles['wrapper__link']} link={actionLink} label={actionLabel} onDark /> }
     </div>
 );
 
