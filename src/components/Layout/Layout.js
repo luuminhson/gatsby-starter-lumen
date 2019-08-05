3509e883aa60a05f5fdf0243120461830f6aa784
Son Luu Minh <son.luu@notabasement.com>
2019-08-05 22:29:56 +0700

    Hide BottomNavigation in post, work, illustration detail page

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -50,7 +50,7 @@ class PureLayout extends React.Component {
     return (
       <div>
         <Navigation burgerClick={this.toggleSidebar} isIndex={isIndex} isPost={isPost} isWork={isWork} pageTitle={pageTitle} onFeaturedImage={hasFeaturedImage} dark={dark} from={from} />
-        <BottomNavigation className={styles['bottomNav']} bottomNav={bottomNav} />
+        { ( isPost || isWork || isIllussion ) ? null : <BottomNavigation className={styles['bottomNav']} bottomNav={bottomNav} /> }
         <div
           className={[
             styles['layout'],
